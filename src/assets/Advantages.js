cbc20ccbe6fbf1d46b03ccbea8d9c82fe1414ac5
Krupali Lalakiya <krupali.lalakiya@gmail.com>
2025-04-09 18:22:44 +0530

    changes in path of image

--- src/assets/Advantages.js
+++ src/assets/Advantages.js
@@ -1,19 +1,19 @@
 const Advantages = [
   {
     id: 1,
-    icon: "/public/icon1.png",
+    icon: "/icon1.png",
     title: "Easy to Order",
     description: "Your Favourite Food delivery Partner. A Trendy Food Delivery",
   },
   {
     id: 2,
-    icon: "/public/icon2.png",
+    icon: "/icon2.png",
     title: "Fast Delivery",
     description: "Your Favourite Food delivery Partner. A Trendy Food Delivery",
   },
   {
     id: 1,
-    icon: "/public/icon3.png",
+    icon: "/icon3.png",
     title: "Secured Payment",
     description: "Your Favourite Food delivery Partner. A Trendy Food Delivery",
   },
